84526144c050b5d90892f97f2c1e63f911d00af0
LexusDev <107411329+LexusDev1@users.noreply.github.com>
2023-08-17 04:03:27 -0700

    Update Music.py

--- cogs/FUN/Music.py
+++ cogs/FUN/Music.py
@@ -10,6 +10,13 @@ class OpMusic(commands.Cog):
     async def Play(self, interaction: nextcord.Interaction, * search: wavelink.YoutubeTrack):
         if not interaction.voice_client:
             vc: wavelink.Player = await interaction.voice.channel.connect(cls: wavelink.Player)
+        elif not interaction.author.voice_client:
+            errEm = nextcord.Embed(description="❌: Join vc first")
+            return interaction.response.send_message(embed=errEm, ephemeral=True)
+        else:
+            vc: wavelink.Player = interaction.voice_client
+        
+        vc.play(search)
 
 def setup(client):
     client.add_cog(Ping(client))
